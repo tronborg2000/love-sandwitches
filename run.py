27c4dafb4c1509a73db365210d29cabee07a3cfc
tronborg2000 <colinweldon1980@gmail.com>
2022-12-11 19:26:13 +0000

    update function

--- run.py
+++ run.py
@@ -54,27 +54,9 @@ def validate_data(values):
         return False
 
     return True
+ 
 
-
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")   
-
- def update_worksheet(data, worksheet):
+def update_worksheet(data, worksheet):
     """
     Receives a list of integers to be inserted into a worksheet
     Update the relevant worksheet with the data provided
